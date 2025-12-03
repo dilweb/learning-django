5cb97ca7e407ab285d09007e5fa116146d43030f
Dilyaver <yxs.crevier@gmail.com>
2025-12-03 10:56:33 +0500

    Added Order models.py

--- learning_django/shopapp/models.py
+++ learning_django/shopapp/models.py
@@ -7,4 +7,10 @@ class Product(models.Model):
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
 
+
+class Order(models.Model):
+    delivery_address = models.TextField(null=True, blank=True)
+    promo = models.CharField(max_length=20, null=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
